c05e36f016b22353d78be05a602ca6bee059be6e
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-06-02 20:46:13 +1000

    Updated database tables

--- backend/app/sql/create_tables.sql
+++ backend/app/sql/create_tables.sql
@@ -1,67 +1,147 @@
--- Active: 1747886189739@@127.0.0.1@3306@awe_electronics
-DROP TABLE IF EXISTS `Reports`;
-DROP TABLE IF EXISTS `Receipts`;
-DROP TABLE IF EXISTS `Invoices`;
+# Active: 1747886189739@@127.0.0.1@3306@awe_electronics
+# No transaction as the whole point of this file is to overwrite the database
+
+# Overwrite existing tables
+DROP TABLE IF EXISTS `OrderItem`;
 DROP TABLE IF EXISTS `Trolley`;
-DROP TABLE IF EXISTS `Accounts`;
-DROP TABLE IF EXISTS `Orders`;
-DROP TABLE IF EXISTS `Lineitems`;
-DROP TABLE IF EXISTS `Products`;
+DROP TABLE IF EXISTS `Product-Image`;
+DROP TABLE IF EXISTS `Product-Tag`;
+DROP TABLE IF EXISTS `Report`;
+DROP TABLE IF EXISTS `Receipt`;
+DROP TABLE IF EXISTS `Invoice`;
+DROP TABLE IF EXISTS `LineItem`;
+DROP TABLE IF EXISTS `Order`;
+DROP TABLE IF EXISTS `Address`;
+DROP TABLE IF EXISTS `Account`;
+DROP TABLE IF EXISTS `Product`;
+DROP TABLE IF EXISTS `Image`;
+DROP TABLE IF EXISTS `Tag`;
 DROP TABLE IF EXISTS `Status`;
-DROP TABLE IF EXISTS `Roles`;
+DROP TABLE IF EXISTS `Role`;
 
-CREATE TABLE `Roles` (
-	`roleID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`name` VARCHAR(50) NOT NULL
-) ENGINE=InnoDB;
+# Start node tables
+# (Tables that contain no foreign keys and are referenced as foreign keys in other tables)
 
-CREATE TABLE `Status` (
-	`statusID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `Tag` (
+	`tagID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(32) NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE `Products` (
+CREATE TABLE `Image` (
+	`imageID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`url` VARCHAR(32) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE `Product` (
 	`productID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
+	`name` VARCHAR(100) NOT NULL,
+	`description` VARCHAR(200) NOT NULL,
+	`price` FLOAT NOT NULL,
+	`stock` INT NOT NULL COMMENT 'This is the quantity of this product currently held.',
+	`available` INT NOT NULL COMMENT 'This is the quantity of this product available for purchase.',
+	`creationDate` DATETIME NOT NULL,
+	`discontinued` INT(2) NOT NULL COMMENT 'Is this item no longer being sold.'
 ) ENGINE=InnoDB;
 
-CREATE TABLE `Lineitems` (
-	`lineItemID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+# Intermediate node tables
+# (Tables that contain foreign keys and are referenced as foreign keys in other tables)
 
-) ENGINE=InnoDB;
-
-CREATE TABLE `Orders` (
-	`orderID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-) ENGINE=InnoDB;
-
-CREATE TABLE `Accounts` (
+CREATE TABLE `Account` (
 	`accountID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`role` enum('owner', 'admin', 'employee', 'customer', 'guest') NOT NULL,
+	`status` enum('active', 'inactive', 'deactive') NOT NULL,
+	`firstname` VARCHAR(50) DEFAULT NULL,
+	`lastname` VARCHAR(50) DEFAULT NULL,
 	`email` VARCHAR(100) NOT NULL,
 	`password` VARCHAR(100) NOT NULL,
+	`creationDate` DATETIME NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE `Address` (
+	`addressID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`accountID` INT NOT NULL,
+	`location` VARCHAR(150) NOT NULL,
+	CONSTRAINT `address_FK_account` FOREIGN KEY (`accountID`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE `Order` (
+	`orderID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`accountID` INT NOT NULL,
+	`addressID` INT NOT NULL,
+	`date` DATETIME NOT NULL,
+	CONSTRAINT `order_FK_account` FOREIGN KEY (`accountID`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `order_FK_address` FOREIGN KEY (`addressID`) REFERENCES `Address` (`addressID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE `LineItem` (
+	`lineItemID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`productID` INT NOT NULL,
+	`quantity` INT NOT NULL,
+	`priceAtSale` FLOAT DEFAULT NULL,
+	CONSTRAINT `lineItem_FK_product` FOREIGN KEY (`productID`) REFERENCES `Product` (`productID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+# End node tables
+# (Tables that contain foreign keys and are not referenced as foreign keys in other tables)
+
+CREATE TABLE `Invoice` (
+	`invoiceID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`accountID` INT NOT NULL,
+	`orderID` INT NOT NULL,
 	`creationDate` DATETIME NOT NULL,
-	`roleID` INT NOT NULL,
-	`statusID` INT NOT NULL,
-	CONSTRAINT `accounts_FK_roles`FOREIGN KEY `roleID` REFERENCES `roles` (`roleID`) ON UPDATE RESTRICT ON DELETE RESTRICT
-	CONSTRAINT `accounts_FK_status`FOREIGN KEY `statusID` REFERENCES `status` (`statusID`) ON UPDATE RESTRICT ON DELETE RESTRICT
+	`data` BLOB NOT NULL,
+	CONSTRAINT `invoice_FK_account` FOREIGN KEY (`accountID`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `invoice_FK_order` FOREIGN KEY (`orderID`) REFERENCES `Order` (`orderID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE `Receipt` (
+	`receiptID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`accountID` INT NOT NULL,
+	`orderID` INT NOT NULL,
+	`creationDate` DATETIME NOT NULL,
+	`data` BLOB NOT NULL,
+	CONSTRAINT `receipt_FK_account` FOREIGN KEY (`accountID`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `receipt_FK_order` FOREIGN KEY (`orderID`) REFERENCES `Order` (`orderID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE `Report` (
+	`reportID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`creator` INT NOT NULL COMMENT 'AccountID',
+	`creationDate` DATETIME NOT NULL,
+	`data` BLOB NOT NULL,
+	CONSTRAINT `report_FK_account` FOREIGN KEY (`creator`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+# Weak entities
+
+CREATE TABLE `Product-Tag` (
+	`productID` INT NOT NULL,
+	`tagID` INT NOT NULL,
+	PRIMARY KEY (`productID`, `tagID`),
+	CONSTRAINT `product-tag_FK_product` FOREIGN KEY (`productID`) REFERENCES `Product` (`productID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `product-tag_FK_tag` FOREIGN KEY (`tagID`) REFERENCES `Tag` (`tagID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+CREATE TABLE `Product-Image` (
+	`productID` INT NOT NULL,
+	`imageID` INT NOT NULL,
+	PRIMARY KEY (`productID`, `imageID`),
+	CONSTRAINT `product-image_FK_product` FOREIGN KEY (`productID`) REFERENCES `Product` (`productID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `product-image_FK_image` FOREIGN KEY (`imageID`) REFERENCES `Image` (`imageID`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
 
 CREATE TABLE `Trolley` (
-	`trolleyID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
+	`accountID` INT NOT NULL,
+	`lineItemID` INT NOT NULL,
+	PRIMARY KEY (`accountID`, `lineItemID`),
+	CONSTRAINT `trolley_FK_account` FOREIGN KEY (`accountID`) REFERENCES `Account` (`accountID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `trolley_FK_lineItem` FOREIGN KEY (`lineItemID`) REFERENCES `LineItem` (`lineItemID`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
 
-CREATE TABLE `Invoices` (
-	`invoiceID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-) ENGINE=InnoDB;
-
-CREATE TABLE `Receipts` (
-	`receiptID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-) ENGINE=InnoDB;
-
-CREATE TABLE `Reports` (
-	`reportID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
+CREATE TABLE `OrderItem` (
+	`orderID` INT NOT NULL,
+	`lineItemID` INT NOT NULL,
+	PRIMARY KEY (`orderID`, `lineItemID`),
+	CONSTRAINT `order-item_FK_account` FOREIGN KEY (`orderID`) REFERENCES `Order` (`orderID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `order-item_FK_lineItem` FOREIGN KEY (`lineItemID`) REFERENCES `LineItem` (`lineItemID`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB;
