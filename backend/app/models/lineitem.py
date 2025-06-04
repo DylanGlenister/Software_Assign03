c4e05ce6f28e75392e8f4cfb7220470a53e1dab7
Big-Oat <162939826+Big-Oat@users.noreply.github.com>
2025-06-04 12:32:18 +1000

    adding back lost classes woopsie
    Fixed file structure adding back lost classes
    
    woopsie

---
+++ backend/app/models/lineitem.py
@@ -0,0 +1,2 @@
+class LineItem: 
+    def __init__(self, product,):
---
+++ backend/app/models/order.py
@@ -0,0 +1,9 @@
+class Order:
+    def __init__(self, order_id, trolley, customer):
+        self.order_id = order_id
+        self.trolley = trolley
+        self.customer = customer
+        
+    def saveOrder:
+    
+    def getOrderDetails: 
