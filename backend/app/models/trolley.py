effbb6840c434a413eb79955c436de52e2e0a224
Big-Oat <162939826+Big-Oat@users.noreply.github.com>
2025-06-05 18:18:10 +1000

    Updated trolley

--- backend/app/models/trolley.py
+++ backend/app/models/trolley.py
@@ -4,28 +4,29 @@ from .lineitem import LineItem
 
 class Trolley:
     def __init__(self, db: Database, accountID: int):
-        self.lineItems = []
+        self.lineItems = db.get_trolley(accountID)
 
-    def addLineItem(self, productID):
-        quantity = 1
-        for lineItem in self.lineItems:
-            if lineItem.productID == productID:
-                lineItem.quantity += quantity
-                return
-        self.lineItems.append(lineItem(productID, quantity))
-        return
-    
-    def UpdateQuantity(self, lineItem: LineItem, newQuantity: int):
-        for lineItem in self.lineItems:
-            if newQuantity <= 0:
-                self.lineItems.remove(lineItem)
-            else:
-                lineItem.quantity = newQuantity
-            return True 
-        return False
-    
-    def totalCost(self):
-        return sum(lineItem.priceAtPurchase for lineItem in self.LineItems)
-    
-    def getLineItems(self):
-        return
+        def addLineItem(self, productID):
+            quantity = 1
+            for lineItem in self.lineItems:
+                if lineItem["productID"] == productID:
+                    lineItem["quantity"] += quantity
+                    db.change_quantity_of_product_in_trolley(accountID, productID, quantity)
+                    return
+            db.add_to_trolley(accountID, productID, quantity)
+            return
+        
+        def UpdateQuantity(self, productID: int, newQuantity: int):
+            for lineItemID in self.lineItems:
+                if newQuantity <= 0:
+                    db.remove_from_trolley(accountID, lineItem.lineItemId)
+                else:
+                    lineItem.quantity = newQuantity
+                return True 
+            return False
+        
+        def totalCost(self):
+            return sum(lineItem.priceAtPurchase for lineItem in self.LineItems)
+        
+        def getLineItems(self):
+            return
