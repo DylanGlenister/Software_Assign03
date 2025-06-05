bc2fbd3de1d6d8d5e07cdb40cf32b150fa053d81
Big-Oat <162939826+Big-Oat@users.noreply.github.com>
2025-06-05 15:09:08 +1000

    Removed line item
    Removed line item

--- backend/app/models/trolley.py
+++ backend/app/models/trolley.py
@@ -1,27 +1,27 @@
 
 class Trolley:
-    def __init__(self):
-        self.lineItems = []
+    def __init__(self, accountId: int):
 
-    def addLineItem(self, product):
-        quantity = 1
-        for lineItem in self.lineItems:
-            if lineItem.product.productID == product.poroductID:
-                lineItem.quantity += quantity
-                return
-        self.lineItems.append(lineItem(product, quantity))
-    
-    def UpdateQuantity(self, productID, newQuantity):
-        for lineItem in self.lineItems:
-            if newQuantity <= 0:
-                self.lineItems.remove(lineItem)
-            else:
-                lineItem.quantity = newQuantity
-            return True 
-        return False
-    
-    def totalCost(self):
-        return sum(lineItem.price for lineItem in self.LineItems)
-    
-    def getLineItems(self):
-        return
+
+        def addLineItem(self, productId):
+            quantity = 1
+            for lineItem in self.lineItems:
+                if lineItem.product.productID == product.poroductID:
+                    lineItem.quantity += quantity
+                    return
+            self.lineItems.append(lineItem(product, quantity))
+        
+        def UpdateQuantity(self, productID, newQuantity):
+            for lineItem in self.lineItems:
+                if newQuantity <= 0:
+                    self.lineItems.remove(lineItem)
+                else:
+                    lineItem.quantity = newQuantity
+                return True 
+            return False
+        
+        def totalCost(self):
+            return sum(lineItem.price for lineItem in self.LineItems)
+        
+        def getLineItems(self):
+            return  a<
