d697308adbc4769f7a1d31f75c7f3f96ba0eae9d
Big-Oat <162939826+Big-Oat@users.noreply.github.com>
2025-06-05 17:05:38 +1000

    Merge branch 'main' of https://github.com/DylanGlenister/Software_Assign03

--- backend/app/models/lineitem.py
+++
@@ -1,8 +0,0 @@
-from .core.database import Database
-
-class LineItem: 
-    def __init__(self, productId: int, quanity: int, db: Database, priceAtPurchase: float):
-
-        self.productID = productId
-        self.quanity = quanity
-        self.priceAtPurchase = priceAtPurchase
