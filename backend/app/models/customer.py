4b5ea5ccec07d3f3764e86de43cd54ac0632eb0c
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-06-05 17:38:29 +1000

    Updating customer class

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -1,18 +1,22 @@
-from pydantic import EmailStr
 from datetime import datetime
 from uuid import uuid4
-from fastapi import HTTPException, status as httpStatus
 
-from .account import Account
+from fastapi import HTTPException
+from fastapi import status as httpStatus
+from pydantic import EmailStr
+
 from ..core.database import Database, Role, Status
+from .account import Account
+
 
 class CustomerAccount(Account):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @classmethod
     def register(cls, db: Database, email: EmailStr, password: str, role: Role, status: Status) -> dict:
         """Create a new account with hashed password."""
-        existing: dict = db.get_account(email=email)
+        existing: dict | None = db.get_account(_email=email)
         if existing:
             raise HTTPException(
                 status_code=httpStatus.HTTP_409_CONFLICT,
@@ -33,23 +37,23 @@ class CustomerAccount(Account):
 
         hashed_password: str = cls._hash_password(password)
         email = email.strip().lower()
-        creation_date: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        creation_date: datetime = datetime.now()
 
-        accountID: int = db.create_account(email, hashed_password, creation_date, role, status)
+        accountID: int = db.create_account(role, email, hashed_password, _creationDate=creation_date)
         if accountID is None:
             raise HTTPException(
                 status_code=httpStatus.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An unknown issue caused account creation to fail."
             )
 
-        return {"account": cls(accountID, email, hashed_password, creation_date, role=role, status=status)}
-    
+        return {"account": cls(accountID, email, hashed_password, creation_date.strftime("%Y-%m-%d %H:%M:%S"), role=role, status=status)}
+
     @classmethod
     def create_guest(cls, db: Database) -> dict:
         guest_email = f"guest_{uuid4().hex[:8]}@temp.domain"
         creation_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        accountID: int = db.create_account(guest_email, "", creation_date, role=Role.GUEST, status=Status.ACTIVE)
+        accountID: int = db.create_account()
         if accountID is None:
             raise HTTPException(
                 status_code=httpStatus.HTTP_500_INTERNAL_SERVER_ERROR,
