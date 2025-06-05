6703429aa37d9b313aa857839ae259bccd48dfb7
Big-Oat <162939826+Big-Oat@users.noreply.github.com>
2025-06-05 22:15:55 +1000

    updated order

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -1,16 +1,21 @@
-from .core.database import Database
+from ..core.database import Database
 from .trolley import Trolley
 
 class orderManager:
     def __init__(self, accountId: int, addressId, db: Database, trolley: list):
+        self.accountId: int = accountId 
+        self.addressId: int = addressId
+        self.trolley: list = trolley
+        self.db: Database = db
+    
+    def createorder(self):
+        orderId = self.db.create_order(self.accountId, self.addressId, self.trolley)
+        return orderId
+    
+    def save_invoice(self, orderId):
+        result = self.db.save_invoice(self.accountId, orderId)
+        return result 
+    def save_receipt(self, orderId):
+        result = self.db.save_receipt(self.accountId, orderId)
+        return result 
         
-        def saveOrder(self):
-            orderId = db.create_order(accountId, addressId, trolley)
-            return orderId
-        
-        def save_invoice(self, orderId):
-            result = db.save_invoice(accountId, orderId)
-            return result 
-        def save_receipt(self, orderId):
-            result = db.save_receipt(accountId, orderId)
-            return result 
